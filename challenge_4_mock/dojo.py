4fd0341942aeb413153bb4491772a67ab7180e14
Du Yuyang <du.r.yuyang@gmail.com>
2016-11-04 11:46:10 +1100

    update

--- challenge_4_mock/dojo.py
+++ challenge_4_mock/dojo.py
@@ -8,20 +8,21 @@ class Buyahouse(object):
             house = self.get_random_house('melbourne')
         return house
 
-    def get_random_house(self, city):
-        pass
 
-
-    def evaluate(self, houses):
+    def evaluate(self, houses, budget):
+        if len(houses) == 0:
+            raise Exception("Nothing Available!")
         for house in houses:
-            r = requests.get('http://realestate.com/%s' % house)
+            try:
+                r = requests.get('http://realestate.com/%s' % house)
+            except ConnectionError:
+                return 'Boom!'
             info = self.get_house_info(r.text)
-            if house_is_undervalued(info):
-                if under_budget(info):
+            if self.house_is_undervalued(info):
+                if self.get_price(info) < budget:
                     self.send_email(house, info)
                 else:
                     self.add_to_watch_list(house)
-
 
 
 
@@ -30,3 +31,22 @@ class Buyahouse(object):
             return json.loads(open(filename).read())
         except (IOError, ValueError):
             return "Error"
+
+
+    def get_house_info(self, text):
+        pass
+
+    def house_is_undervalued(self, info):
+        pass
+
+    def get_price(self, info):
+        pass
+
+    def send_email(self, house, info):
+        pass
+
+    def add_to_watch_list(self, house):
+        pass
+
+    def get_random_house(self, city):
+        pass
